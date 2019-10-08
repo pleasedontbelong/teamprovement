a9f98872c86592d5991bf435671dd26a57e210b2
Abel André <abel_andre@ultimatesoftware.com>
2019-10-08 17:45:56 +0200

    add meeting list view and meeting detail view

--- teamprovement/apps/meeting/urls.py
+++ teamprovement/apps/meeting/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from .views import MeetingListView
+from django.urls import path
+
+from .views import MeetingListView, MeetingDetailView
 
 urlpatterns = [
-    url(r'^', MeetingListView.as_view(), name='meeting_list'),
+    url(r'^$', MeetingListView.as_view(), name='meeting_list'),
+    path('<int:pk>', MeetingDetailView.as_view(), name='meeting_detail'),
 ]
--- teamprovement/apps/meeting/views.py
+++ teamprovement/apps/meeting/views.py
@@ -1,10 +1,19 @@
-from django.views.generic import ListView, TemplateView
+from django.views.generic import TemplateView, DetailView
+
 from .models import Meeting
 
 
-# class MeetingListView(ListView):
-#     model = Meeting
-#     template_name = "meeting/list.jinja2"
-
 class MeetingListView(TemplateView):
     template_name = "meeting/list.jinja2"
+
+    def get_context_data(self):
+        return {'meetings': Meeting.objects.all()}
+
+
+class MeetingDetailView(DetailView):
+    template_name = "meeting/detail.jinja2"
+    model = Meeting
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
