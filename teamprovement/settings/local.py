ed5da0f78a0951ca2e8e530e9428c65ec17cc6cf
Pablo Leano <pleasedontbelong@gmail.com>
2019-10-08 16:51:32 +0200

    Remove local settings from git

--- teamprovement/settings/local.py
+++
@@ -1,8 +0,0 @@
-import dj_database_url
-
-from .base import *  # NOQA
-
-DEBUG = True
-SHELL_PLUS = "ipython"
-
-DATABASES['default'] = dj_database_url.parse('postgres://teamprov:teamprov@localhost:5432/teamprov', conn_max_age=600)
