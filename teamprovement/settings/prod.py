ba6042343f01cae78648fe17a982d070b84c50a0
Pablo Leano <pleasedontbelong@gmail.com>
2019-10-09 09:26:47 +0200

    Prod config

--- teamprovement/settings/prod.py
+++ teamprovement/settings/prod.py
@@ -1,11 +1,22 @@
-# https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+import dj_database_url
 
 from .base import *  # NOQA
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-# PARENT_HOST = 'mysite.com'
-# ALLOWED_HOSTS = ['.%s' % PARENT_HOST]
-# SESSION_COOKIE_DOMAIN = ALLOWED_HOSTS[0]
-# SESSION_COOKIE_NAME = 'mysite_sid'
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600)
+}
+
+INSTALLED_APPS += [
+    'raven.contrib.django.raven_compat',
+    'storages',
+]
+
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE
+
+PARENT_HOST = 'teamprovement.herokuapp.com'
+ALLOWED_HOSTS = ['.%s' % PARENT_HOST]
+SESSION_COOKIE_DOMAIN = ALLOWED_HOSTS[0]
+SESSION_COOKIE_NAME = 'teamprovement-cookie'
